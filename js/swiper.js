deb8666f709fc884214cd2f169a196642835c401
EricVartanov <82024638+EricVartanov@users.noreply.github.com>
2022-06-26 14:17:46 +0300

    Add files via upload

--- js/swiper.js
+++ js/swiper.js
@@ -11,6 +11,7 @@ const swiper1 = new Swiper(".idc-swiper-1", {
     /*    grabCursor: true, */
     watchOverflow: true,
     grabCursor: true,
+    autoHeight: true,
 
     pagination: {
         el: ".idc-swiper-1 .idc-swiper-pagination",
@@ -27,7 +28,7 @@ const swiper2 = new Swiper(".idc-swiper-2", {
     // Default parameters
     loop: false,
     direction: "horizontal",
-    slidesPerView: 3,
+    slidesPerView: 1,
     spaceBetween: 10,
     watchSlidesVisibility: true,
     watchSlidesProgress: true,
@@ -36,13 +37,36 @@ const swiper2 = new Swiper(".idc-swiper-2", {
     /*    grabCursor: true, */
     watchOverflow: true,
     grabCursor: true,
-    grid: {
-        fill: "row",
-        rows: 2,
-    },
+
     pagination: {
         el: ".idc-swiper-2 .idc-swiper-pagination",
         type: "bullets",
         clickable: true,
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        700: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+            grid: {
+                fill: "row",
+                rows: 3,
+            },
+        },
+        1020: {
+            slidesPerView: 3,
+            spaceBetween: 17,
+            grid: {
+                fill: "row",
+                rows: 2,
+            },
+        },
+
+        1340: {
+            spaceBetween: 10,
+        },
     },
 });
